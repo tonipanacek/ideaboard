524cee69567bdce6ddd6a8c5e313c2c88a56e596
Toni Panacek <toni.panacek@gmail.com>
2018-10-31 18:43:09 +0100

    added idea component and connected api endpoint

---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Idea from './Idea'
+
+class IdeasContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/ideas.json')
+    .then(response => {
+      console.log(response)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button className="newIdeaButton" onClick={this.addNewIdea} >
+            New Idea
+          </button>
+        </div>
+        {this.state.ideas.map((idea) => {
+          return(<Idea idea={idea} key={idea.id} />)
+        })}
+      </div>
+      );
+  }
+}
+
+export default IdeasContainer
