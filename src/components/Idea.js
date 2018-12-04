6269b76f8616f7d22f19c00b92ae99c67a030a2c
Toni Panacek <toni.panacek@gmail.com>
2018-12-04 12:15:28 +0100

    added option to delete idea tile

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -6,9 +6,15 @@ class Idea extends Component {
     this.props.onClick(this.props.idea.id)
   }
 
+  handleDelete = () => {
+    this.props.onDelete(this.props.idea.id)
+  }
+
   render() {
     return (
       <div className="tile">
+        <span className="deleteButton" onClick={this.handleDelete}>
+        x </span>
         <h4 onClick={this.handleClick}>{this.props.idea.title}</h4>
         <p onClick={this.handleClick}>{this.props.idea.body}</p>
       </div>
