1f2e967b92e0f3a9ce4eb385d466cb8400144cb9
Toni Panacek <toni.panacek@gmail.com>
2018-12-04 11:59:01 +0100

    able to edit all tiles

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -11,6 +11,7 @@ class IdeaForm extends Component {
   }
 
   handleInput = (e) => {
+    this.props.resetNotification()
     this.setState({[e.target.name]: e.target.value})
   }
 
@@ -27,6 +28,7 @@ class IdeaForm extends Component {
       })
     .then(response => {
       console.log(response)
+      this.props.updateIdea(response.data)
     })
     .catch(error => console.log(error))
   }
@@ -37,7 +39,8 @@ class IdeaForm extends Component {
         <form onBlur={this.handleBlur} >
           <input className='input' type='text'
             name='title' placeholder='Enter a Title'
-            value={this.state.title} onChange={this.handleInput} />
+            value={this.state.title} onChange={this.handleInput}
+            ref={this.props.titleRef} />
           <textarea className='input' name='body'
             placeholder='Describe your idea'
             value={this.state.body} onChange={this.handleInput}>
--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -9,7 +9,8 @@ class IdeasContainer extends Component {
     super(props)
     this.state = {
       ideas: [],
-      editingIdeaId: null
+      editingIdeaId: null,
+      notification: ''
     }
   }
 
@@ -45,6 +46,25 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+    this.setState({
+      ideas: ideas,
+      notification: 'All changes saved'
+    })
+  }
+
+  resetNotification = () => {
+    this.setState({notification: ''})
+  }
+
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id},
+      () => { this.title.focus() })
+  }
 
   render() {
     return (
@@ -53,12 +73,18 @@ class IdeasContainer extends Component {
           <button className="newIdeaButton" onClick={this.addNewIdea} >
             New Idea
           </button>
+          <span className="notification">
+            {this.state.notification}
+          </span>
         </div>
         {this.state.ideas.map((idea) => {
           if(this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} />)
+            return(<IdeaForm idea={idea} key={idea.id}
+              updateIdea={this.updateIdea}
+              titleRef={input => this.title = input}
+              resetNotification={this.resetNotification} />)
           } else {
-          return(<Idea idea={idea} key={idea.id} />)
+          return(<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />)
           }
         })}
       </div>
